0192f973fdf840ef479a7fb9378a9f8a9dfdd721
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-05-14 20:38:49 +0900

    change port

--- server/app.js
+++ server/app.js
@@ -252,7 +252,7 @@ socket.on("translate_stt_message",data=> {
 
 // httpsServer.listen
 
-httpServer.listen(80, () => {
+httpServer.listen(8080, () => {
   console.log('HTTP Server is running at 8080!');
   console.log("server is healthy")
 });
